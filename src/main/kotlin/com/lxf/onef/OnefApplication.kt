fcddb8e4530aea2fa69df25b5ca88554d823fce0
linxinfu <xinfu.lin@orgneering.com>
2019-10-29 15:43:47 +0800

    初始化

---
+++ src/main/kotlin/com/lxf/onef/OnefApplication.kt
@@ -0,0 +1,11 @@
+package com.lxf.onef
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnefApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnefApplication>(*args)
+}
---
+++ src/test/kotlin/com/lxf/onef/OnefApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lxf.onef
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnefApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
